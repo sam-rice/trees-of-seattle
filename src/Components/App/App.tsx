ddd1a909105008c33e87d2995ee7021b59ed28ed
Sam Rice <samsrice@gmail.com>
2023-01-10 15:51:38 -0800

    Initial commit

---
+++ src/Components/App/App.tsx
@@ -0,0 +1,13 @@
+import React from "react"
+import { Routes, Route } from "react-router-dom"
+import "./_App.scss"
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Working</h1>
+    </div>
+  )
+}
+
+export default App
