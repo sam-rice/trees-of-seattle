8d497040190f10e941799107e06e960bc594bff3
Sam Rice <samsrice@gmail.com>
2023-01-11 17:16:17 -0800

    add Leaflet and co-dependencies

--- src/Components/MapView/MapView.tsx
+++ src/Components/MapView/MapView.tsx
@@ -1,4 +1,5 @@
 import { Outlet } from "react-router-dom"
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet"
 
 import "./_MapView.scss"
 
@@ -7,7 +8,22 @@ const MapView = () => {
     <>
       <main className="map-main">
         <div className="map-container">
-
+        <MapContainer
+          center={[47.626395, -122.329386]}
+          zoom={12}
+          scrollWheelZoom={false}
+          style={{height: "50vh"}}
+        >
+          <TileLayer
+            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          />
+          <Marker position={[47.606209, -122.332069]}>
+            <Popup>
+              A pretty CSS3 popup. <br /> Easily customizable.
+            </Popup>
+          </Marker>
+        </MapContainer>
         </div>
       </main>
 
